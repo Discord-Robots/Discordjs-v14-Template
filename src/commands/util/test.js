7a5440bd40e868ecbca8f27aa1c3b2c19693c6ba
D3ord3NidAm <georgeneedham7@gmail.com>
2022-09-03 03:08:55 +0000

    Change test.js to show example of embedPage Handle

--- src/commands/util/test.js
+++ src/commands/util/test.js
@@ -1,31 +1,61 @@
 const {
-    Client,
-    SlashCommandBuilder,
-    CommandInteraction,
-    EmbedBuilder
+  Client,
+  SlashCommandBuilder,
+  CommandInteraction,
+  EmbedBuilder,
 } = require("discord.js");
 const ms = require("ms");
 
 module.exports = {
-    developer: true,
-    category: "util",
-    data: new SlashCommandBuilder()
-        .setName("test")
-        .setDescription("Returns an embed."),
-    /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @returns
-     */
-    async execute(interaction, client) {
-        await interaction.deferReply({
-            fetchReply: true,
-            ephemeral: true
-        })
-        let message = `Test command works`
-
-        client.utils.successEditEmbed(message, interaction)
-
-    },
+  developer: true,
+  category: "util",
+  data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("Returns an embed."),
+  /**
+   *
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @returns
+   */
+  async execute(interaction, client) {
+    const embeds = [
+      new EmbedBuilder({
+        title: "Test 1",
+        description: "Description 1",
+        footer: {
+          text: "This is a cool embed paginator!",
+        },
+      }),
+      new EmbedBuilder({
+        title: "Test 2",
+        description: "Description 2",
+        footer: {
+          text: "This is a cool embed paginator!",
+        },
+      }),
+      new EmbedBuilder({
+        title: "Test 3",
+        description: "Description 3",
+        footer: {
+          text: "This is a cool embed paginator!",
+        },
+      }),
+      new EmbedBuilder({
+        title: "Test 4",
+        description: "Description 4",
+        footer: {
+          text: "This is a cool embed paginator!",
+        },
+      }),
+      new EmbedBuilder({
+        title: "Test 5",
+        description: "Description 5",
+        footer: {
+          text: "This is a cool embed paginator!",
+        },
+      }),
+    ];
+    await client.embedPages(interaction, embeds);
+  },
 };
