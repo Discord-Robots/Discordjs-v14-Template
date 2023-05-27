4d4e0d04f2cf945f611c5afea1a38a63b7658bb9
Peter-MJ-Parker <georgeneedham7@gmail.com>
2023-05-27 19:38:11 +0000

    feat: add instructions to cmd handler

--- src/functions/handlers/commandHandler.js
+++ src/functions/handlers/commandHandler.js
@@ -1,36 +1,41 @@
-const { DevGuild } = process.env;
+/**
+ * If you want to create commands for use in ONLY one server:
+ * uncomment lines 6 and 36-39 and comment out line 32.
+ * And be sure to set DevGuild/Channel in .env
+ */
+// const { DevGuild } = process.env;
 /**
  *
  * @param {import("../../Structures/bot")} client
  */
 module.exports = (client) => {
-  const { commands, commandArray, developerArray, rds } = client;
-  client.handleCommands = async () => {
-    const commandFolders = rds("./src/commands");
-    for (const folder of commandFolders) {
-      const commandFiles = rds(`./src/commands/${folder}`).filter((file) =>
-        file.endsWith(".js")
-      );
+    const { commands, commandArray, developerArray, rds } = client;
+    client.handleCommands = async () => {
+        const commandFolders = rds("./src/commands");
+        for (const folder of commandFolders) {
+            const commandFiles = rds(`./src/commands/${folder}`).filter((file) =>
+                file.endsWith(".js")
+            );
 
-      for (const file of commandFiles) {
-        const command = require(`../../commands/${folder}/${file}`);
+            for (const file of commandFiles) {
+                const command = require(`../../commands/${folder}/${file}`);
 
-        if (command.developer) {
-          developerArray.push(command.data.toJSON());
-        } else {
-          commandArray.push(command.data.toJSON());
+                if (command.developer) {
+                    developerArray.push(command.data.toJSON());
+                } else {
+                    commandArray.push(command.data.toJSON());
+                }
+                commands.set(command.data.name, command);
+            }
         }
-        commands.set(command.data.name, command);
-      }
-    }
 
-    //Global Commands!
-    await client.application.commands.set(commandArray);
+        //Global Commands!
+        await client.application.commands.set(commandArray);
 
-    //Single Guild Commands!
-    if (DevGuild) {
-      const devGuild = client.guilds.cache.get(DevGuild);
-      devGuild.commands.set(developerArray);
-    }
-  };
+        //Single Guild Commands!
+        // if (DevGuild) {
+        //   const devGuild = client.guilds.cache.get(DevGuild);
+        //   devGuild.commands.set(developerArray);
+        // }
+    };
 };
