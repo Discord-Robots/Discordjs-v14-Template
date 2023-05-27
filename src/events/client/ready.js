77093dbe7fef7e45a09e92426400287e64b15324
Peter-MJ-Parker <georgeneedham7@gmail.com>
2023-05-27 19:31:26 +0000

    refactor: fix ready event

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,23 +2,23 @@ const { Connect } = process.env;
 const blockedGuids = require("../../models/blocked");
 
 module.exports = {
-  name: "ready",
-  /**
-   *
-   * @param {import("../../Structures/bot")} client
-   */
-  async execute(client) {
-    if (Connect) {
-      let db = await blockedGuids.findOne({ client_id: client.user.id });
-      if (!db) {
-        new blockedGuids({
-          client_id: client.user.id,
-          guilds: [],
-        })
-          .save()
-          .catch(console.error());
-      }
-      client.pickPresence();
-    }
-  },
+    name: "ready",
+    /**
+     *
+     * @param {import("../../Structures/bot")} client
+     */
+    async execute(client) {
+        if (Connect) {
+            let db = await blockedGuids.findOne({ client_id: client.user.id });
+            if (!db) {
+                new blockedGuids({
+                    client_id: client.user.id,
+                    guilds: [],
+                })
+                    .save()
+                    .catch(console.error());
+            }
+            client.pickPresence();
+        }
+    },
 };
