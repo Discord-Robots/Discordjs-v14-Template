e741c2ce32dc4ac51b600480400175b028fa1f15
Peter-MJ-Parker <georgeneedham7@gmail.com>
2023-05-27 19:36:23 +0000

    refactor: prettify code

--- src/functions/handlers/eventHandler.js
+++ src/functions/handlers/eventHandler.js
@@ -3,21 +3,21 @@
  * @param {import("../../Structures/bot")} client
  */
 module.exports = (client) => {
-  const { events, rds } = client;
-  client.removeAllListeners();
-  client.handleEvents = async () => {
-    const eventFolders = rds(`./src/events`);
-    for (const folder of eventFolders) {
-      const eventFiles = rds(`./src/events/${folder}`).filter((file) =>
-        file.endsWith(".js")
-      );
-      for (const file of eventFiles) {
-        const event = require(`../../events/${folder}/${file}`);
-        const execute = (...args) => event.execute(...args, client);
-        if (event.once) client.once(event.name, execute);
-        else client.on(event.name, execute);
-        events.set(event.name, execute);
-      }
-    }
-  };
+	const { events, rds } = client;
+	client.removeAllListeners();
+	client.handleEvents = async () => {
+		const eventFolders = rds(`./src/events`);
+		for (const folder of eventFolders) {
+			const eventFiles = rds(`./src/events/${folder}`).filter((file) =>
+				file.endsWith('.js')
+			);
+			for (const file of eventFiles) {
+				const event = require(`../../events/${folder}/${file}`);
+				const execute = (...args) => event.execute(...args, client);
+				if (event.once) client.once(event.name, execute);
+				else client.on(event.name, execute);
+				events.set(event.name, execute);
+			}
+		}
+	};
 };
